5b4d3e1faa600aaf33eeda0ab35e63dcf1e25669
bacetech <ciro.cavalleri@gmail.com>
2021-08-14 01:59:17 +0200

    add select and input for generating a qr code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React, { useState } from 'react';
 import QRCode from 'react-qr-code';
 import './App.css';
 
 function App() {
+  const [urlQr, setUrlQr] = useState('https://www.tenutafontana.com/alberata');
   return (
-    <div className="App">
-      <QRCode value="http://www.tenutafontana.com/alberata" />
+    <div className="App" style={{ margin: '5px' }}>
+      <h2>Proposte:</h2>
+      <select onChange={(e) => setUrlQr(e.target.value)}>
+        <option value="https://www.tenutafontana.com/alberata">
+          Alberata - Tenuta Fontana
+        </option>
+        <option value="https://www.tenutafontana.com/shop/index.php?route=product/product&path=63&product_id=64">
+          Alberata - Tenuta Fontana Shop
+        </option>
+        <option value="https://www.youtube.com/watch?v=B80_T55OeDw">
+          Alberata - Tenuta Fontana Youtube
+        </option>
+      </select>
+      <div style={{ margin: '15px' }}>
+        <QRCode value={urlQr} />
+      </div>
+      <h2>Visualizza o inserisci url:</h2>
+      <input
+        type="text"
+        value={urlQr}
+        onChange={(e) => setUrlQr(e.target.value)}
+        style={{ width: '400px' }}
+      />
     </div>
   );
 }
